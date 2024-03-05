f840aadb50499a0a0aa30ebb4bedf2dfc095ab8e
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-03-05 05:57:12 -0700

    Added user authentication and registration functionality

---
+++ Back-end/src/main/java/admin_user/loginRegistrationApp.java
@@ -0,0 +1,13 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class loginRegistrationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(loginRegistrationApp.class, args);
+	}
+
+}
