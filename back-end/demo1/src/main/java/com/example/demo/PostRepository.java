644f9fb863b13b6b278486a101720030a67c395b
Rohit <156241286+rohitb82003@users.noreply.github.com>
2024-03-12 15:12:07 -0600

    Delete back-end/demo1 directory

--- back-end/demo1/src/main/java/com/example/demo/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-    // Define querying methods here
-}
