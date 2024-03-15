d2627fe7ae8da5bd1a0b6acffaed40c448589ed3
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-03-15 09:51:29 -0600

    update file

--- back-end/RegisterLogin/src/main/java/com/example/RegisterLogin/Config/SecurityConfig.java
+++ back-end/RegisterLogin/src/main/java/com/example/RegisterLogin/Config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class  SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
