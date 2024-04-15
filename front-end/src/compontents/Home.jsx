84641f1c1050d4ba0b1e39facbd4b1794404df08
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-04-14 20:31:11 -0600

    minor changes

---
+++ front-end/src/compontents/Home.jsx
@@ -0,0 +1,14 @@
+// Home.jsx
+
+import React from 'react';
+import './comm.css'; // Import the CSS file
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome</h1>
+    </div>
+  );
+}
+
+export default Home;
