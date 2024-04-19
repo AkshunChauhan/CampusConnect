380eeb6ec27b76175141c4893c50b4f77cb0b25c
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-04-18 18:39:09 -0600

    update after feedback

--- Calender/src/main/java/com/example/calender/model/CalendarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.calender.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {
-
-}
